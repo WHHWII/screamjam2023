5dc5aa4f0b844377b337cd3f9172f16850b42f2c
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-10-17 14:34:12 -0500

    Character Controller

---
+++ scripts/CharacterController.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class CharacterController : CharacterBody3D
+{
+	[Export] public Camera3D camera;
+	float lookSensitivity = (float)ProjectSettings.GetSetting("player/look_sensitivity");
+
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+		if (@event is InputEventMouseMotion)
+		{
+			var inputMotion = (@event as InputEventMouseMotion);
+			RotateY(-inputMotion.Relative.X * lookSensitivity);
+			camera.RotateX(-inputMotion.Relative.Y * lookSensitivity);
+			//camera.Rotation = Mathf.Clamp(camera.Rotation.X, -Math.PI / 2, Math.PI / 2);
+
+		}
+
+
+
+	}
+}
