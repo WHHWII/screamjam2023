5ebd60fcdf2a87eed12cf6845db7df9ba5327a8b
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-10-20 22:14:22 -0500

    myer updates

--- scripts/Michael.cs
+++ scripts/Michael.cs
@@ -7,21 +7,32 @@ public partial class Michael : Node3D
 	[Export] CharacterController player;
 	[Export] public Timer visibleToPlayerTimer;
 	[Export] public Vector3 hidePoint;
+	[Export] public Vector3 showPoint;
 	[Export] public RayCast3D playerLOSRay;
 	public bool hiding = false;
+	public bool aproaching = false;
 	public float hideSpeed;
 
 	bool playerInLOS = false;
-
+	bool onPlayerScreen = false;
 	public override void _Ready()
 	{
 		hidePoint = Position;
 	}
 	public override void _PhysicsProcess(double delta)
 	{
-		if (!hiding)
+		LookAt(player.Position);
+		if (aproaching)
 		{
-			LookAt(player.Position);
+			GlobalPosition = GlobalPosition.MoveToward(showPoint, hideSpeed * (float)delta);
+			if (GlobalPosition.IsEqualApprox(showPoint))
+			{
+				aproaching = false;
+			}
+		}
+		else if (!hiding)
+		{
+			
 			playerLOSRay.LookAt(new Vector3(player.Position.X, player.Position.Y + 0.5f, player.Position.Z));
 			var targetCol = playerLOSRay.GetCollider();
 			if (targetCol != null && targetCol.GetType() == typeof(CharacterController))
@@ -42,38 +53,56 @@ public partial class Michael : Node3D
 			}
 		}
 
-		
+		if (playerInLOS && onPlayerScreen)
+		{
+			if (visibleToPlayerTimer.Paused)
+			{
+				GD.Print("myer spotted");
+				visibleToPlayerTimer.Start();
+				visibleToPlayerTimer.Paused = false;
+			}
+		}
+		else if (!visibleToPlayerTimer.Paused)
+		{
+			GD.Print("lost sight of myer");
+			visibleToPlayerTimer.Paused = true;
+		}
 
 
 
 	}
 
+	public void MyerWrapper()
+	{
+		_on_visible_on_screen_notifier_3d_screen_exited();
+		_on_visible_on_screen_notifier_3d_screen_entered();
 
+	}
 
 	private void _on_visible_on_screen_notifier_3d_screen_entered()
 	{
-		if (playerInLOS)
-		{
-			GD.Print("myer spotted");
-			visibleToPlayerTimer.Start();
-			visibleToPlayerTimer.Paused = false;
-		}
+		onPlayerScreen = true;
 	}
 
 
 	private void _on_visible_on_screen_notifier_3d_screen_exited()
 	{
-		GD.Print("lost sight of myer");
-		visibleToPlayerTimer.Paused = true; 
+
+		onPlayerScreen = false;
 		// Replace with function body.
 	}
 	private void _on_timer_timeout()
 	{
 		GD.Print("Myer is fleeing");
-		GD.Print(hidePoint);
 		hiding = true;
-		LookAt(hidePoint);
+		//LookAt(hidePoint);
 	}
 }
+
+
+
+
+
+
 
 
--- scripts/MyerPoint.cs
+++ scripts/MyerPoint.cs
@@ -9,6 +9,9 @@ public partial class MyerPoint : Node3D
 	[Export] public Michael myer;
 	[Export] public float hideSpeed = 200;
 	[Export] public float stareTime = 1;
+	[Export] public bool approachFromHidePoint;
+	bool hasBeenTriggered;
+	bool isActive;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,18 +21,42 @@ public partial class MyerPoint : Node3D
 
 	private void _on_area_3d_body_entered(Node3D body)
 	{
-		myer.GlobalPosition = spawnPoint.GlobalPosition;
+		if (hasBeenTriggered) return;
+		hasBeenTriggered = true;
+		isActive = true;
 		myer.hidePoint = hidePoint.GlobalPosition;
+		myer.showPoint = spawnPoint.GlobalPosition;
 		myer.hiding = false;
 		myer.Visible = true;
+		
 		myer.hideSpeed = hideSpeed;
 		myer.visibleToPlayerTimer.WaitTime = stareTime;
+		myer.visibleToPlayerTimer.Stop();
+		myer.visibleToPlayerTimer.Paused = true;
+		if (approachFromHidePoint)
+		{
+			myer.GlobalPosition = hidePoint.GlobalPosition;
+			myer.aproaching = true;
+		}
+		else
+		{
+			myer.GlobalPosition = spawnPoint.GlobalPosition;
+			myer.aproaching = false;
+		}
 
-
+		myer.MyerWrapper();
 		GD.Print("Myer has moved");
 		// Replace with function body.
+
+	}
+	private void _on_myer_flee_area_body_entered(Node3D body)
+	{
+		myer.hiding = true;
 	}
 }
+
+
+
 
 
 
